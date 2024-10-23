9a4182c4c9cbdc866792fe0b4b4ef0ada8bf5d7f
Riaz Gul <riazgul03@gmail.com>
2024-10-23 09:47:13 -0700

    Link & Navigation In Next.js

--- next-app/src/app/about/page.js
+++ next-app/src/app/about/page.js
@@ -1,7 +1,14 @@
-const About =()=>{
-    return(
+"use client"
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+const About = () => {
+    const router = useRouter();
+    return (
         <div>
             <h1>About Page</h1>
+            <br />
+            <button onClick={() => router.push("/")}>Go to Home Page</button>
+
         </div>
     )
 }
--- next-app/src/app/login/page.js
+++ next-app/src/app/login/page.js
@@ -1,6 +1,12 @@
-const Login= () =>{
-    return(
-        <h1>Login Page</h1>
+import Link from "next/link"
+
+const Login = () => {
+    return (
+        <>
+            <h1>Login Page</h1>
+            <br />
+            <Link href="/">Go to Home Page</Link>
+        </>
     )
 }
 export default Login
