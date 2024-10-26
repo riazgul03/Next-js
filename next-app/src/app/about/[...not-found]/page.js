453c7277a73cb5d44d141d6450c1d3370c6e92e6
Riaz Gul <riazgul03@gmail.com>
2024-10-26 09:42:29 -0700

    4O4 Page Not-Found in Next.js

---
+++ next-app/src/app/about/[...not-found]/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NotAboutFound() {
+    return (
+        <>
+            <h1>This About Page is Not Avaiable</h1>
+            <br /><br />
+            <Link href="/">Click Here To home Page</Link>
+        </>
+    )
+}
