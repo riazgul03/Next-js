9708abdf22c24a801ccdd9af3ab2b67e526a8047
Riaz Gul <riazgul03@gmail.com>
2024-10-28 10:21:21 -0700

    CSS Module With Next.js

--- next-blog.js/src/app/page.js
+++ next-blog.js/src/app/page.js
@@ -1,11 +1,12 @@
-
-import Link from "next/link";
-
+import Custom from './custom.module.css'
+import Other from './other.module.css'
+import Outside from './style/outside.module.css'
 export default function Home() {
   return (
     <div>
-      <h1>Style and CSS with Next js</h1>
-      <h2>Heading 2 in main page</h2>
+      <h1 className={Custom.main}>CSS Module With Next js</h1>
+      <h2 className={Other.main} >Heading Other  CSS Module</h2>
+      <h3 className={Outside.main}>Outside Module CSS text</h3>
     </div>
   );
 }
