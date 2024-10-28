e252c9023a45490457ee763dad70f097399f3464
Riaz Gul <riazgul03@gmail.com>
2024-10-28 09:42:44 -0700

    Style with Next.js

--- next-blog.js/src/app/page.js
+++ next-blog.js/src/app/page.js
@@ -1,12 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>Fetch Data With API in Client Component</h1>
-      <Link href="/serverapilist">Go To Server API List List</Link>
+      <h1>Style and CSS with Next js</h1>
+      <h2>Heading 2 in main page</h2>
     </div>
   );
 }
