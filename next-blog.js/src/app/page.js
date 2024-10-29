e5b2705a347f9be602f4e93a93f46da720972873
Riaz Gul <riazgul03@gmail.com>
2024-10-29 10:10:44 -0700

    GenerateMetadata For Dynamic Meta Data in Next.js

--- next-blog.js/src/app/page.js
+++ next-blog.js/src/app/page.js
@@ -1,15 +1,8 @@
-import { Roboto } from "next/font/google"
-const roboto = Roboto({
-  weight: '100',
-  subsets: ['latin'],
-  display: 'swap'
-})
+
 export default function Home() {
   return (
     <div>
-      <h1>Font Optimization in Next.js</h1>
-      <h1 className={roboto.className}>Font With Next js Font Feature</h1>
-      {/* <h1 style={{fonFamily:'Roboto', fontWeight:'100'}}>Font With Link Tag</h1> */}
+      <h1>GenerateMetadata in Next js</h1>
     </div>
   );
 }
