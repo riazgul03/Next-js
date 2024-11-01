c02fd64186f933daa514ee38ca32706380e3a123
Riaz Gul <riazgul03@gmail.com>
2024-11-01 08:25:02 -0700

    Export Static HTML Page With Build in next.js

--- next-blog.js/src/app/page.js
+++ next-blog.js/src/app/page.js
@@ -1,8 +1,13 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>Make Production Build in Next js</h1>
+      <h1>Export Static HTML With Build in Next js</h1>
+      <Link href="/products">Go To Products Page</Link>
+      <br /><br />
+      <Link href="/seller">Go To Seller Page</Link>
+
     </div>
   );
 }
