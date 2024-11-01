dcd1562c5153a4fdeaea8b516ae971ef32bebe55
Riaz Gul <riazgul03@gmail.com>
2024-11-01 10:37:37 -0700

    error

--- next-blog.js/src/app/page.js
+++ next-blog.js/src/app/page.js
@@ -3,11 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Export Static HTML With Build in Next js</h1>
-      <Link href="/products">Go To Products Page</Link>
-      <br /><br />
-      <Link href="/seller">Go To Seller Page</Link>
-
+     <h1>SSG in Next JS</h1>
     </div>
   );
 }
