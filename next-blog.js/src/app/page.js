e567f82e02883ae4dda54f237ee1d0294cc12b76
Riaz Gul <riazgul03@gmail.com>
2024-11-02 11:18:30 -0700

    Enviroment Variable in Next.js

--- next-blog.js/src/app/page.js
+++ next-blog.js/src/app/page.js
@@ -1,9 +1,16 @@
 import Link from "next/link";
 
 export default function Home() {
+  console.log(process.env.NODE_ENV);
   return (
     <div>
-      <h1>Redirection In Next JS</h1>
+      {
+        process.env.NODE_ENV == "development" ?
+          <h1>You Are in Development Mode</h1>
+          :
+          <h1>You Are Production Mode</h1>
+      }
+      <h1>Enviroment Variables In Next JS</h1>
     </div>
   );
 }
