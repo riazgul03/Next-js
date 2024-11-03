3315ee3207e8b474e76f215b07a2ee64285bf315
Riaz Gul <riazgul03@gmail.com>
2024-11-03 09:50:25 -0800

    Call API Which Create in ApI Routes in Next.js

--- next-api/src/app/api/users/[id]/route.js
+++ next-api/src/app/api/users/[id]/route.js
@@ -5,5 +5,5 @@ export function GET(requst, content) {
 
     const userData = user.filter((item) => item.id == content.params.id)
 
-    return NextResponse.json(userData.length == 0 ? { result: "No Page Found", success: false } : { result: userData, success: true }, { status: 200 })
+    return NextResponse.json(userData.length == 0 ? { result: "No Page Found", success: false } : { result: userData[0], success: true }, { status: 200 })
 }
