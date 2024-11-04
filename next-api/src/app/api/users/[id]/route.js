1f7ece818fbcc8aed93b068d769bc10285498d03
Riaz Gul <riazgul03@gmail.com>
2024-11-04 08:10:26 -0800

    Make PUT API and Route in Next.js

--- next-api/src/app/api/users/[id]/route.js
+++ next-api/src/app/api/users/[id]/route.js
@@ -7,3 +7,13 @@ export function GET(requst, content) {
 
     return NextResponse.json(userData.length == 0 ? { result: "No Page Found", success: false } : { result: userData[0], success: true }, { status: 200 })
 }
+
+export async function PUT(request, content) {
+    let payload = await request.json();
+    payload.id = content.params.id;
+    console.log(payload);
+    if (!payload.id || !payload.name || !payload.age || !payload.age || !payload.email) {
+        return NextResponse.json({ result: 'request data not vaild', success: false }, { status: 400 })
+    }
+    return NextResponse.json({ result: payload, success: true }, { status: 200 })
+}
