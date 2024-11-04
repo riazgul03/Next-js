88c4dc97cf891e49b7915ce4f06555967a6d7662
Riaz Gul <riazgul03@gmail.com>
2024-11-04 10:49:13 -0800

    Integrate PUT API And Routes in Next.js

--- next-api/src/app/users/page.js
+++ next-api/src/app/users/page.js
@@ -1,5 +1,7 @@
+
 import Link from "next/link";
 
+import './../style.css'
 async function getUsers() {
     let data = await fetch("http://localhost:3000/api/users");
     data = await data.json();
@@ -14,8 +16,9 @@ export default async function Page() {
             <h1>User List</h1>
             {
                 users.map((item) => (
-                    <div>
-                        <Link href={`users/${item.id}`}>{item.name}</Link>
+                    <div className="user-item">
+                        <span><Link href={`users/${item.id}`}>{item.name}</Link></span>
+                        <span> <Link href={`users/${item.id}/update `}>Edit</Link></span>
                     </div>
                 ))
             }
