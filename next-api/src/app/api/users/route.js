6d3343f8fec16ae1184c561be28545fc07030071
Riaz Gul <riazgul03@gmail.com>
2024-11-03 19:26:31 -0800

    Make API With POSt Method in Next.js

--- next-api/src/app/api/users/route.js
+++ next-api/src/app/api/users/route.js
@@ -5,3 +5,12 @@ export function GET() {
     const data = user;
     return NextResponse.json(data, { status: 200 })
 }
+
+export async function POST(request) {
+    let payload = await request.json();
+    console.log(payload.name);
+    if (!payload.name || !payload.age || !payload.email) {
+        return NextResponse.json({ result: "require field note found", success: false }, { status: 400 })
+    }
+    return NextResponse.json({ result: "User new Created", success: true }, { status: 200 })
+}
